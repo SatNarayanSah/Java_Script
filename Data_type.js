7d5116eac38c9e431220323627d09acc6ac08c13
sat narayan sah <76737029+SatNarayanSah@users.noreply.github.com>
2023-06-26 07:21:21 +0545

    Prime no.

---
+++ Data_type.js
@@ -0,0 +1,10 @@
+// Streing type variable
+var myName="Satyam sah"; //declraing string type variablle "myName".
+console.log(myName);  //caling myName variable to print there value.
+console.log(typeof(myName));  //finding which type of variable is that.
+
+// undefind data type
+var User; //declraing string type variablle "myName".
+console.log(User);  //caling myName variable to print there value.
+console.log(typeof(User));  //finding which type of variable is that.
+
---
+++ control_statements_&_LOOP.js
@@ -0,0 +1,10 @@
+//Programe that check instilized data is prime or not.......
+var n1=5;     //declaring variable and have value.
+if(n1%2==1) //condetion to find that number is prime number or not...
+{
+    console.log("Given data is priem number");   //if value is true....
+}
+ else
+ {
+    console.log("Given number is not prime.."); // if value is false
+ }
