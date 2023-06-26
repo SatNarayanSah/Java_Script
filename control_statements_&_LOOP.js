bf7b7b3dae98c03d79ee763346d1350439df26c5
sat narayan sah <76737029+SatNarayanSah@users.noreply.github.com>
2023-06-26 07:34:48 +0545

    Leap year

--- control_statements_&_LOOP.js
+++ control_statements_&_LOOP.js
@@ -1,10 +1,27 @@
 //Programe that check instilized data is prime or not.......
-var n1=5;     //declaring variable and have value.
-if(n1%2==1) //condetion to find that number is prime number or not...
-{
-    console.log("Given data is priem number");   //if value is true....
+// var n1=5;     //declaring variable and have value.
+// if(n1%2==1) //condetion to find that number is prime number or not...
+// {
+//     console.log("Given data is priem number");   //if value is true....
+// }
+//  else
+//  {
+//     console.log("Given number is not prime.."); // if value is false
+//  }
+
+ //Write a programme to find given number is leap year or not.
+ var t=2014; 
+if(t%4==0){ 
+    if(t%100==0)
+    {
+        console.log("The givn number "+ t + " is not leap year.....");
+    }
+    else
+    {
+        console.log("The givn number "+ t + " is  leap year.....");
+    }
+    
 }
- else
- {
-    console.log("Given number is not prime.."); // if value is false
- }
+else{
+    console.log("The givn number "+ t + " is not leap year.....");
+}
