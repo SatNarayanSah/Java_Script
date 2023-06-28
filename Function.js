ea2c325c79110aa9b856c9c8a4cdbd63b02602b4
sat narayan sah <76737029+SatNarayanSah@users.noreply.github.com>
2023-06-28 18:46:21 +0545

    Table of 3

--- Function.js
+++ Function.js
@@ -7,19 +7,43 @@ consists of the func tion keyword, followed by: */
 A list of parametes to the function, enclosed in parantheses and seperate ny commas(,) 
 The javascript statements that define the function, enclosed in curly brackets,{......} */
 
-function sun()   // Function declaring
-{
-    var a=5, b=6;
-    var  total=a+b;
- console.log(total);
-}
-sun();    //calling function
+// function sun()   // Function declaring
+// {
+//     var a=5, b=6;
+//     var  total=a+b;
+//  console.log(total);
+// }
+// sun();    //calling function
 
 /* write a progrme using function parameter nad arguments */
-function sum(a,b){  // In Function sum "a and b " is called paremeters
-var total = a+b;
-console.log(total);
+// function sum(a,b){  // In Function sum "a and b " is called paremeters
+// var total = a+b;
+// console.log(total);
+// }
+// sum();
+// sum(12,12);  // In calling Function sum the values " 12 and 12" are called arguments
+// sum(100000,52000000);    // In calling Function sum the values " 100000 and 52000000" are called arguments
+
+
+
+/*  FUNCTION EXPRESSION  */
+// function sum(a,b){  // In Function sum "a and b " is called paremeters
+//     var total = a+b;
+//     console.log(total);
+//     }
+
+//     var funExp = sum(12,12);  
+//     funExp;   // this is function expression do the work.
+
+//     /*  FUNCTION EXPRESSION  WITH RETURN KEYWORD */
+// function sum(a,b){  // In Function sum "a and b " is called paremeters
+//         return total = a+b;  // Using Return keyword
+//     }
+//     var funExp = sum(12,12);  
+//     console.log("This is the sum of given two numbers:- " + funExp);   
+
+var n=1;
+for(n; n<=10;n++){
+    tableOf=3;
+    console.log(tableOf + " * "  + n + " = " + tableOf*n);
 }
-sum();
-sum(12,12);  // In calling Function sum the values " 12 and 12" are called arguments
-sum(100000,52000000);    // In calling Function sum the values " 100000 and 52000000" are called arguments
