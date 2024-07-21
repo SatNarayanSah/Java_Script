0962cfe7ddfb98d994631c1a75d795536fee1c7f
Satyam Sah <76737029+SatNarayanSah@users.noreply.github.com>
2024-07-21 17:09:56 +0545

    new learnig way

--- index2.js
+++ index2.js
@@ -1,1 +1,30 @@
-console.log('Thi')
+// let a=null; //this null premitive data type is also knowns as object due to its historical bug
+// let b = 345; //Number premitive datatype
+// let c = true; //Boolean Premitive datatype
+// let d = BigInt("678");  //Bigint preimtive datatype
+// let e = "Satyam"; //
+// let f = Symbol("I am a webdeveloper;");
+// let g = undefined;
+// console.log(a,b,c,d,e,f,g);
+// console.log("type of a  " + typeof a);
+// console.log("type of b  " + typeof b);
+// console.log("type of c  " + typeof c);
+// console.log("type of d  " + typeof d);
+// console.log("type of e  " + typeof e);
+// console.log("type of f  " + typeof f);
+// console.log("type of g  " + typeof g);
+
+
+// Non premitive datatype objects in JS
+
+// const item = {
+//   "satyam": true,
+//   "sah": false,
+//   "harry": 344,
+//   "code": undefined
+// }
+// console.log(item["satyam"]);
+// console.log(item["sah"]);
+// console.log(item["harry"]);
+// console.log(item["code"]);
+
