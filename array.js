23df609cc8da289f21ecced83e56a96b9e3309b7
Satyam Sah <76737029+SatNarayanSah@users.noreply.github.com>
2024-08-20 10:17:38 +0545

    kjgdfhjir

---
+++ array.js
@@ -0,0 +1,42 @@
+let Class_BIT = [1,3,5,6,7, "students", "rools"];
+// console.log(Class_BIT[5]);
+// console.log("The total elemts in Class_BIT array is ", Class_BIT.length);
+// Class_BIT[7]="satyam";
+// console.log("after adding new elemts on array the length of array is ",Class_BIT.length,"\n and the value in that index is", Class_BIT[7]);
+// for(let i=0; i<Class_BIT.length; i++){
+//   console.log("The value of array at index Class_BIT [",i,"]", "= ", Class_BIT[i]);  
+// }
+
+
+
+// Methods of array
+
+// let b= Class_BIT.toString();
+// console.log(b,"\nthe data type of it is:", typeof b);
+// let c = Class_BIT.join(" , ");
+// console.log(c, typeof c);
+// Class_BIT.pop();
+//  console.log(Class_BIT);
+//  Class_BIT.push("Teachers");
+// console.log(Class_BIT);
+// Class_BIT.shift();
+// console.log(Class_BIT);
+
+
+let NewArray = [3,5,1,5,6];
+console.log(Class_BIT.length);
+delete Class_BIT[2];
+console.log(Class_BIT.length);
+console.log(Class_BIT);
+let concated = Class_BIT.concat(NewArray);
+console.log(NewArray);
+console.log(concated);
+concated.sort();
+console.log(concated);
+
+
+
+
+
+
+ 
